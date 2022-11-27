029d3f14c365a9d283a0a93cc6ca21415dee642e
Zak Pruitt <pruittzn@gmail.com>
2022-11-27 02:31:06 -0600

    update function

--- Application/parser.py
+++ Application/parser.py
@@ -11,14 +11,15 @@ file_name = './Application/mist-highpass-65645aa69059.json'
 creds = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
 client = gspread.authorize(creds)
 
-# Fetch the sheet
-sheet = client.open('Mist Guild - Illidan Reagent Tracker').sheet1
-reagent_df = pd.DataFrame(sheet.get_all_records())
-idx = reagent_df.index[reagent_df['Character'] == 'based'][0]
-reagent_df.at[idx,'item1']='new cascsac'
-sheet.update([reagent_df.columns.values.tolist()] + reagent_df.values.tolist())
-
 
 class Parser:
     def __init__(self) -> None:
-        self.reagent_dictionary = sheet.get_all_records()
+        sheet = client.open('Mist Guild - Illidan Reagent Tracker').sheet1
+        reagent_df = pd.DataFrame(sheet.get_all_records())
+
+    def update_reagent_count(self, character, item, count):
+        idx = self.reagent_df.index[self.reagent_df['Character']
+                                    == character][0]
+        self.reagent_df.at[idx, item] = count
+        self.sheet.update(
+            [self.reagent_df.columns.values.tolist()] + self.reagent_df.values.tolist())
